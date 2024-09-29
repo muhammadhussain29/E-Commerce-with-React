b7016fdfbe5151db7765558c158cd8d34e24cd1e
muhammadhussain29 <hussainmughal29@gmail.com>
2024-09-29 11:19:43 +0500

    product images added | products content updated

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import item1 from "../Media/dress-shirt-1.jpeg"
-import item2 from "../Media/polo-shirt-1.png"
 
-const ProductCard = () => {
+const ProductCard = (props) => {
     return (
-        <div class="card bg-zinc-200 relative w-[280px] h-[400px] rounded-[20px] overflow-hidden before:bg-orange-500 before:absolute before:top-[-50%] before:w-full before:h-full before:skew-y-[345deg] before:transition hover:before:top-[- 70%] hover:before:skew-y-[390deg]">
-            <div class="relative w-full flex justify-center items-center pt-5 z-10 ">
-                <img src={item2} alt="mouse corsair" class="h-[200px] w-auto" />
+        <div className="card bg-zinc-200 relative w-[280px] h-[360px] rounded-[20px] overflow-hidden before:bg-orange-500 before:absolute before:top-[-50%] before:w-full before:h-full before:skew-y-[345deg] before:transition hover:before:top-[- 70%] hover:before:skew-y-[390deg]">
+            <div className="relative w-full flex justify-center items-center pt-4 z-10 ">
+                <img src={props.data.image} alt="mouse corsair" className="h-[190px] w-auto transition" />
             </div>
-            <div class="content relative top-[50px] transition-all p-5 flex justify-center items-center flex-col z-20">
-                <h3 className='text-lg font-medium uppercase'>Mouse Corsair M65</h3>
-                <h2 class="price">61.<small>98</small> €</h2>
-                <h2 class="price">Stars</h2>
-                <button class="relative top-[100px] opacity-0 py-2 px-5 bg-orange-500 text-white mt-4 rounded-3xl uppercase transition">Buy Now</button>
+            <div className="content relative top-[30px] transition-all pt-4 flex justify-center items-center flex-col z-20">
+                <h3 className='text-lg font-medium uppercase text-zinc-700'>{props.data.name}</h3>
+                <h2 className="text-zinc-700">{props.data.price} Rs</h2>
+                <h2 className="text-zinc-700">Stars {props.data.stars}</h2>
+                <button className="relative top-[100px] opacity-0 py-2 px-5 bg-orange-500 hover:bg-orange-700 text-white mt-2 rounded-3xl uppercase transition">Buy Now</button>
             </div>
         </div>
     )
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,18 +1,77 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import polo1 from "../Media/Products/polo-shirt-1.png"
+import tshirt1 from "../Media/Products/t-shirt-1.png"
+import shirt1 from "../Media/Products/dress-shirt-1.png"
+import lawn1 from "../Media/Products/lawn-suit-1.png"
+import lawn2 from "../Media/Products/lawn-suit-2.png"
+import lawn3 from "../Media/Products/lawn-suit-3.png"
+import jeans1 from "../Media/Products/jeans-1.png"
+import watch1 from "../Media/Products/watch-1.png"
+
+const data = [
+    {
+        "name":"Classic Polo",
+        "image":polo1,
+        "price":"700",
+        "stars":"5"
+    },
+    {
+        "name":"Perfect Fit Tee",
+        "image":tshirt1,
+        "price":"400",
+        "stars":"3"
+    },
+    {
+        "name":"Sharp Dress Shirt",
+        "image":shirt1,
+        "price":"1200",
+        "stars":"5"
+    },
+    {
+        "name":"Elegant Lawn",
+        "image":lawn1,
+        "price":"1500",
+        "stars":"5"
+    },
+    {
+        "name":"Vibrant Lawn",
+        "image":lawn2,
+        "price":"1500",
+        "stars":"5"
+    },
+    {
+        "name":"Breezy Lawn",
+        "image":lawn3,
+        "price":"2000",
+        "stars":"5"
+    },
+    {
+        "name":"Versatile Pants",
+        "image":jeans1,
+        "price":"1300",
+        "stars":"3.5"
+    },
+    {
+        "name":"Timeless Watch",
+        "image":watch1,
+        "price":"1000",
+        "stars":"4.2"
+    },
+]
+
 
 const Products = () => {
   return (
-    <div className='md:mx-20 mx-7 my-10'>
-        <h2 className='text-5xl text-orange-500 capitalize font-bold text-center '>Our Catlog</h2>
-        <div className='flex justify-evenly items-center pt-20 flex-wrap gap-10'>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+    <div className='md:mx-20 mx-7 my-10 flex flex-col justify-center'>
+        <h2 className='text-5xl text-orange-500 capitalize font-bold text-center '>Shop the Hottest Styles</h2>
+        <h5 className='text-zinc-700 capitalize text-center text-lg mt-1'>Elevate your wardrobe with these popular, iconic fashion pieces.</h5>
+        <div className='flex justify-evenly items-center pt-14 flex-wrap gap-10'>
+            {data.map((elem,index)=>{
+                return <ProductCard key={index} data={elem} />
+            })}
         </div>
+        <button className="py-4 px-7 bg-orange-500 hover:bg-orange-700 text-white mt-10 rounded-full uppercase mx-auto ">View Catalog</button>
     </div>
   )
 }
